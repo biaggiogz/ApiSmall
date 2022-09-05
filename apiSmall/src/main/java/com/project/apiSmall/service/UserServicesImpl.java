6a826991ce7e29282def6519212856a74b2872e4
= <biaggiogzdev@gmail.com>
2022-09-05 17:16:46 +0200

    [inTerface] create UserService | [service] create UserServicesImpl

---
+++ apiSmall/src/main/java/com/project/apiSmall/service/UserServicesImpl.java
@@ -0,0 +1,12 @@
+package com.project.apiSmall.service;
+
+
+import com.project.apiSmall.dataTransfer.UserDTO;
+import com.project.apiSmall.inTerface.UserService;
+
+public class UserServicesImpl implements UserService {
+    @Override
+    public UserDTO createUser(UserDTO userdto) {
+        return null;
+    }
+}
