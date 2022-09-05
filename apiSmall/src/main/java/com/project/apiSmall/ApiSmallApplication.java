82296c8fccae2c3dbdf7ece8cec27479ac387db2
biaggio <biaggiogzdev@gmail.com>
2022-09-05 14:02:13 +0200

    start project

---
+++ apiSmall/src/main/java/com/project/apiSmall/ApiSmallApplication.java
@@ -0,0 +1,13 @@
+package com.project.apiSmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSmallApplication.class, args);
+	}
+
+}
